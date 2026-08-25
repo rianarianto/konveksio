3d02a179f500b5dc46ec3f79b472ee63d2df9cb5
rianarianto <spc@rianarianto.local>
2026-08-26 01:21:04 +0700

    feat: automatically seed default 6 production stages whenever a new shop is created

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -26,5 +26,23 @@ class Shop extends Model
     protected static function booted(): void
     {
         static::addGlobalScope(new \App\Models\Scopes\ShopScope);
+
+        static::created(function (Shop $shop) {
+            $defaultStages = [
+                ['name' => 'Potong', 'base_wage' => 1000, 'order_sequence' => 1, 'for_produksi_custom' => true, 'for_non_produksi' => false, 'for_jasa' => true],
+                ['name' => 'Jahit', 'base_wage' => 3000, 'order_sequence' => 2, 'for_produksi_custom' => true, 'for_non_produksi' => false, 'for_jasa' => true],
+                ['name' => 'Bordir/Sablon', 'base_wage' => 2000, 'order_sequence' => 3, 'for_produksi_custom' => true, 'for_non_produksi' => true, 'for_jasa' => true],
+                ['name' => 'Kancing', 'base_wage' => 500, 'order_sequence' => 4, 'for_produksi_custom' => true, 'for_non_produksi' => false, 'for_jasa' => true],
+                ['name' => 'Finishing', 'base_wage' => 500, 'order_sequence' => 5, 'for_produksi_custom' => true, 'for_non_produksi' => true, 'for_jasa' => true],
+                ['name' => 'QC', 'base_wage' => 500, 'order_sequence' => 6, 'for_produksi_custom' => true, 'for_non_produksi' => true, 'for_jasa' => true],
+            ];
+
+            foreach ($defaultStages as $stage) {
+                \App\Models\ProductionStage::firstOrCreate(
+                    ['shop_id' => $shop->id, 'name' => $stage['name']],
+                    $stage
+                );
+            }
+        });
     }
 }
